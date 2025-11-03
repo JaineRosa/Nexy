d0c11dea1ce17c499f25b364a96ea663ab3487ac
Jaíne Rosa <jaine.rosa_11@live.com>
2025-11-02 21:20:04 -0300

    Fix: AJustes app.config

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -4,6 +4,5 @@ import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
-bootstrapApplication(App, {
-  providers: [provideRouter(routes)]
-});
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
