5c2f6b21f0140563dbd942bf2a683575c8ca0a18
Jaíne Rosa <jaine.rosa_11@live.com>
2025-11-05 20:55:18 -0300

    atualização pedidos detalhes 1

--- BackEnd/src/main/java/com/example/NEXY/model/PedidoItem.java
+++ BackEnd/src/main/java/com/example/NEXY/model/PedidoItem.java
@@ -2,12 +2,11 @@ package com.example.NEXY.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = {"pedido"})
 public class PedidoItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
