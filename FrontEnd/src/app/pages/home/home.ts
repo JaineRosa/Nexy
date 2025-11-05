397aa736ac8182c68e6e71ed6828f1367a748c3b
Jaíne Rosa <jaine.rosa_11@live.com>
2025-11-04 23:40:52 -0300

    atualização Home

--- FrontEnd/src/app/pages/home/home.ts
+++ FrontEnd/src/app/pages/home/home.ts
@@ -4,11 +4,12 @@ import { Banner } from "../../components/banner/banner";
 import { Cards } from "../../components/cards-produto/cards";
 import { Produto } from '../../interfaces/Produto';
 import { FormsModule } from '@angular/forms';
+import { BuscaCategorias } from "../busca-categorias/busca-categorias";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, Cards, FormsModule, Banner],
+  imports: [CommonModule, Cards, FormsModule, Banner, BuscaCategorias],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
