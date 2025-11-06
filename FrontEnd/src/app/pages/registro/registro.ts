b599c8dbc43a0410d12772a3b2252260d6fe2b91
Jaíne Rosa <jaine.rosa_11@live.com>
2025-11-06 01:34:55 -0300

    atualização rotas

--- FrontEnd/src/app/pages/registro/registro.ts
+++ FrontEnd/src/app/pages/registro/registro.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './registro.html',
   styleUrl: './registro.css'
 })
